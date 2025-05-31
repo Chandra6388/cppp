bf1d914c33407bfad7c436ec09c5b2daf15f8336
Chandra6388 <cppatel6388@gmail.com>
2025-05-31 13:18:40 +0530

    doen

--- FRONTEND/src/hooks/use-auth.tsx
+++
@@ -1,5 +0,0 @@
-
-import { useAuth as useAuthContext } from '@/contexts/AuthContext';
-
-// Re-export the useAuth hook from the context
-export const useAuth = useAuthContext;
--- FRONTEND/src/socket.tsx
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-import * as Config from "../Utils/config";
-
-const socket = io(`${Config.base_url}`);
-
-export default socket;
