b6d666ea907c08757314c0285846e53714862fe4
Chandra6388 <cppatel6388@gmail.com>
2025-06-03 23:54:27 +0530

    done

--- FRONTEND/src/components/modals/TicketDetailModal.tsx
+++ FRONTEND/src/components/modals/TicketDetailModal.tsx
@@ -10,11 +10,6 @@ import { getPriorityColor, getStatusColor, formatDate, getChatTime } from "../..
 import socket from "@/socket";
 import { getChatHistory } from "@/service/employee/ticketService";
 
-
-
-
-
-
 const TicketDetailModal: React.FC<TicketDetailModalProps> = ({ open, onOpenChange, ticket, onStatusChange, setchangestatusModal, changeStatusModal, selectedStatus, setSelectedStatus, handleSubmit, employeeName, type, }) => {
   const [isTyping, setIsTyping] = useState(false);
   const [getAllChats, setAllChat] = useState<Message[]>([]);
